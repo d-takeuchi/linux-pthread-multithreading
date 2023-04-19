0e5b898d082b5fda7199bdbdbb6dbb90c73ff85c
takeuchi <48384909+d-takeuchi@users.noreply.github.com>
2023-04-19 20:10:20 +0900

    プロセス間でメモリは独立しているという例
    varAを子プロセスで変更しているが、あくまでプロセス内でのみ影響にとどまる

--- variablesProc.c
+++ variablesProc.c
@@ -1,3 +1,17 @@
+//--
+//  --実行結果--
+//  main-1: varA=1, varB=2
+//  processFunc -1-1; varA=1, varB=4
+//  processFunc -1-2; varA=5, varB=4
+//  main-2: varA=1, varB=3
+//  processFunc -2-1; varA=1, varB=8
+//  processFunc -2-2; varA=10, varB=8
+//  processFunc -1-3; varA=5, varB=4
+//  processFunc -1-4; varA=5, varB=6
+//  processFunc -2-3; varA=10, varB=8
+//  processFunc -2-4; varA=10, varB=12
+//  main-3: varA=1, varB=3
+//--
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
